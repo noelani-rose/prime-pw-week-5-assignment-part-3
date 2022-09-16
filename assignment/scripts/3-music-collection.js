ea7e068652d5341884f27e8e86c88d9dd2c8b438
Noelani West <noelani.rose14@gmail.com>
2022-09-15 21:50:53 -0500

    created object and function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,22 @@
 console.log('***** Music Collection *****')
+
+ let collection = [];
+
+ const album0 = {
+    title: 'Dreamland',
+    artist: 'Glass Animals',
+    yearPublished: '2020'
+};
+
+function addToCollection (title, artist, yearPublished){
+    collection.push(album0);
+    return collection;
+}
+console.log(addToCollection(collection));
+
+
+
+
+
+collection.push(album);
+console.log(collection);
