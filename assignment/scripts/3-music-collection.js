3ca673c8ca381c7b7965da26d5dbb00d6fb23d81
Noelani West <noelani.rose14@gmail.com>
2022-09-15 23:06:06 -0500

    created 6 albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -7,6 +7,35 @@ console.log('***** Music Collection *****')
     artist: 'Glass Animals',
     yearPublished: '2020'
 };
+const album1 = {
+    title: 'God Loves Ugly',
+    artist: 'Atmosphere',
+    yearPublished: '2002'
+}
+
+const album2 = {
+    title: 'How to: Friend, Love, Freefall',
+    artist: 'Rainbow Kitten Surprise',
+    yearPublished: '2018'
+}
+
+const album3 = {
+    title: 'Good News for People who Love Bad News',
+    artist: 'Modest Mouse',
+    yearPublished: '2004'
+}
+
+const album4 = {
+    title: 'Bayani Redux',
+    artist: 'Blue Scholars',
+    yearPublished: '2007'
+}
+
+const album5 = {
+    title: 'Can\'t Wake Up',
+    artist: 'Shakey Graves',
+    yearPublished: '2018'
+}
 
 function addToCollection (title, artist, yearPublished){
     collection.push(album0);
@@ -15,8 +44,3 @@ function addToCollection (title, artist, yearPublished){
 console.log(addToCollection(collection));
 
 
-
-
-
-collection.push(album);
-console.log(collection);
